f4924d993b90966d2a479ac211bdd9082b7a942d
SIDHANT-SIN <sidhantsingh54321ss@gmail.com>
2025-06-02 14:02:15 +0530

    layout improved

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,49 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Room from './components/Room';
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 import socket from './socket';
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
 
   useEffect(() => {
-  
-    socket.on('connect', () => {
-      console.log('Connected to server');
+    socket.on("connect", () => {
+      console.log("Connected to server");
       setIsConnected(true);
     });
 
-    socket.on('disconnect', () => {
-      console.log('Disconnected from server');
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
       setIsConnected(false);
     });
 
     return () => {
-      socket.off('connect');
-      socket.off('disconnect');
+      socket.off("connect");
+      socket.off("disconnect");
     };
   }, []);
-
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen flex flex-col bg-white">
+        {" "}
+        {/* Changed bg-gray-100 to bg-white */}
         {!isConnected && (
           <div className="bg-red-500 text-white p-2 text-center">
             Disconnected from server
           </div>
         )}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/room/:roomId" element={<Room />} />
-        </Routes>
+        <Navbar />
+        <main className="flex-grow bg-white">
+          {" "}
+          {/* Added bg-white here */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/:roomId" element={<Room />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
